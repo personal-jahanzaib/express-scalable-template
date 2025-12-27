6a9b52b37175b5d5d7b4c5d8e5f2a31211181cab
Jahan Zaib <personal.jahanzaib@gmail.com>
2025-12-27 05:12:42 +0500

    refactor(app): clean up unused variables and improve import formatting

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-import config from "#env";
-import server from "#server";
-import "#syncRoutes";
-
-const a = 1;
-const b = "2";
-console.log(a + b);
+import config from '#env';
+import server from '#server';
+import '#syncRoutes';
 
 server.listen(config.server.port, () => {
   console.info(`Server is running on port ${config.server.port}`);
