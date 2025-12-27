9abf8d4cbb63abe6b495aab66a16a7cb37005e6a
Jahan Zaib <personal.jahanzaib@gmail.com>
2025-12-27 05:12:00 +0500

    feat: add type validation script and enhance nodemon configuration

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-import config from '#env';
-import server from '#server';
-import '#syncRoutes';
+import config from "#env";
+import server from "#server";
+import "#syncRoutes";
+
+const a = 1;
+const b = "2";
+console.log(a + b);
 
 server.listen(config.server.port, () => {
-  // eslint-disable-next-line no-console
   console.info(`Server is running on port ${config.server.port}`);
 });
